ab5d0c622b944193d47a9d823e292b511743add0
truc <trucbntdev7204@gmail.com>
2023-11-10 09:59:31 +0700

    modify cut interface

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php 
 include "view/header.php";
-
+if(isset($_GET['act'])){
+    $act = $_GET['act'];
+    switch($act){
+        case 'about':
+            include "view/about.php";
+            break;
+        case 'service':
+            include "view/service.php";
+            break;
+        case 'menu':
+            include "view/menu.php";
+            break;
+        case 'booking':
+            include "view/booking.php";
+            break;
+        case 'team':
+            include "view/team.php";
+            break;
+        case 'testimonial':
+            include "view/testimonial.php";
+            break;
+    }
+}
 include "view/footer.php";
 ?>
