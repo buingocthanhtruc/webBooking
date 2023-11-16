a35400d7f7cb486839150427ba680b7e5e67f9af
Luong <luongvqph33534@fpt.edu.vn>
2023-11-16 15:54:08 +0700

    Complete html file: quanLySP.php

--- admin/View/quanLySP.php
+++ admin/View/quanLySP.php
@@ -36,7 +36,7 @@
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">X</button>
-                    <button type="button" class="btn btn-success">Gửi</button>
+                    <button type="button" class="btn btn-success">Thêm</button>
                   </div>
                 </div>
               </div>
@@ -69,16 +69,42 @@
                           </svg>
                         </button></a>
 
-                      <a href="">
-                        <button type="button" class="btn rounded btn-success">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-pen" viewBox="0 0 16 16">
-                            <path
-                              d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z">
-                            </path>
-                          </svg>
-                        </button>
-                      </a>
+
+                      <button type="button" class="btn btn-success rounded" data-toggle="modal"
+                        data-target="#suaDanhMuc" data-whatever="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                          class="bi bi-pen" viewBox="0 0 16 16">
+                          <path
+                            d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z">
+                          </path>
+                        </svg>
+                      </button>
+
+                      <div class="modal fade" id="suaDanhMuc" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="exampleModalLabel">Sửa Danh Mục</h5>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                              <form>
+                                <div class="form-group">
+                                  <label for="recipient-name" class="col-form-label">Tên Danh Mục:</label>
+                                  <input type="text" class="form-control" id="recipient-name" value="Đồ Uống">
+                                </div>
+                              </form>
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-dismiss="modal">X</button>
+                              <button type="button" class="btn btn-success">Sửa</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </td>
                   </tr>
 
@@ -130,15 +156,11 @@
                         <label for="recipient-name" class="col-form-label">Giá:</label>
                         <input type="number" class="form-control" id="recipient-name">
                       </div>
-                      <!-- <div class="form-group">
-                        <label for="message-text" class="col-form-label">Message:</label>
-                        <textarea class="form-control" id="message-text"></textarea>
-                      </div> -->
                     </form>
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">X</button>
-                    <button type="button" class="btn btn-success">Gửi</button>
+                    <button type="button" class="btn btn-success">Thêm</button>
                   </div>
                 </div>
               </div>
@@ -178,16 +200,61 @@
                           </svg>
                         </button></a>
 
-                      <a href="">
-                        <button type="button" class="btn btn-success rounded">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-pen" viewBox="0 0 16 16">
-                            <path
-                              d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z">
-                            </path>
-                          </svg>
-                        </button>
-                      </a>
+
+                      <button type="button" class="btn btn-success rounded" data-toggle="modal" data-target="#suaMonAn"
+                        data-whatever="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                          class="bi bi-pen" viewBox="0 0 16 16">
+                          <path
+                            d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z">
+                          </path>
+                        </svg>
+                      </button>
+
+                      <div class="modal fade" id="suaMonAn" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="exampleModalLabel">Sửa Món Ăn</h5>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                              <form>
+                                <div class="form-group">
+                                  <label for="recipient-name" class="col-form-label">Tên Danh Mục:</label>
+                                  <input type="text" class="form-control" id="recipient-name" value="Nuoc Cam">
+                                </div>
+                                <div class="form-group">
+                                  <label for="recipient-name" class="col-form-label">Danh Mục:</label>
+                                  <select class="form-control" name="" id="">
+                                    <option value="Đồ Uống">Đồ Uống</option>
+                                    <option value="Đồ Uống">Món Khai Vị</option>
+                                  </select>
+                                </div>
+                                <div class="form-group">
+                                  <label for="recipient-name" class="col-form-label">Ảnh:</label>
+                                  <input type="file" class="form-control" id="recipient-name" value="Nuoc Cam">
+                                </div>
+                                <div class="form-group">
+                                  <label for="recipient-name" class="col-form-label">Tên Danh Mục:</label>
+                                  <input type="text" class="form-control" id="recipient-name" value="Nuoc Cam">
+                                </div>
+                                <div class="form-group">
+                                  <label for="recipient-name" class="col-form-label">Giá:</label>
+                                  <input type="number" class="form-control" id="recipient-name" value="120000">
+                                </div>
+                              </form>
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-dismiss="modal">X</button>
+                              <button type="button" class="btn btn-success">Sửa</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </td>
                   </tr>
 
